e519aeba2fbce90899512d0426b9ecf01a2f9612
Alex <asigalov61@hotmail.com>
2023-10-15 02:05:37 -0700

    Version 4.5 Further generation speed improvement
    Enjoy! :)
    
    Project Los Angeles
    Tegridy Code 2023

--- euterpe_x_composer.py
+++ euterpe_x_composer.py
@@ -151,7 +151,7 @@ SEQ_LEN = 2048
 model = TransformerWrapper(
     num_tokens = 3344,
     max_seq_len = SEQ_LEN,
-    attn_layers = Decoder(dim = 1024, depth = 32, heads = 8)
+    attn_layers = Decoder(dim = 1024, depth = 32, heads = 8, attn_flash=True)
 )
 
 model = AutoregressiveWrapper(model)
@@ -255,7 +255,7 @@ SEQ_LEN = 2048
 model = TransformerWrapper(
     num_tokens = 3344,
     max_seq_len = SEQ_LEN,
-    attn_layers = Decoder(dim = 1024, depth = 60, heads = 8)
+    attn_layers = Decoder(dim = 1024, depth = 60, heads = 8, attn_flash=True)
 )
 
 model = AutoregressiveWrapper(model)
